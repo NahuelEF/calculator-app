2c45f277034e0e9bb6ad4952b89f1a86ea67a422
NahuelEF <nahuelfedele@gmail.com>
2023-05-22 23:51:38 -0300

    fix: function operations

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
+const buttons = document.querySelectorAll("button");
 const screen = document.querySelector(".screen");
-const numbers = document.querySelectorAll(".keys__number");
-const operators = document.querySelectorAll(".keys__operator");
-const decimal = document.querySelector(".keys__decimal");
-const del = document.querySelector(".keys__delete");
-const equal = document.querySelector(".keys__equal");
 
-const handleClick = (item) => {
-  item.addEventListener("click", () => {
-    screen.value += item.textContent;
+buttons.forEach((key) => {
+  key.addEventListener("click", () => {
+    screen.value === "Error" || screen.value === "undefined"
+      ? (screen.value = "")
+      : "";
+    const keyValue = key.textContent;
+    switch (keyValue) {
+      case "x":
+        screen.value += "*";
+        break;
+
+      case "del":
+        screen.value = screen.value.slice(0, -1);
+        break;
+
+      case "=":
+        try {
+          screen.value = eval(screen.value);
+        } catch {
+          screen.value = "Error";
+        }
+        break;
+
+      default:
+        screen.value += keyValue;
+        break;
+    }
   });
-};
-
-numbers.forEach((number) => {
-  handleClick(number);
-});
-
-operators.forEach((operator) => {
-  handleClick(operator);
-});
-
-handleClick(decimal);
-
-del.addEventListener("click", () => {
-  screen.value = screen.value.slice(0, -1);
-});
-
-equal.addEventListener("click", () => {
-  screen.value = eval(screen.value);
 });
